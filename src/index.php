97508bbe5578bf6685b1c58656ecfde3ec81e707
kenny Callado <kennycallado@hotmail.com>
2021-10-25 20:06:52 +0200

    mejora - Mejora el tratamiento de las rutas - Crea rutas para detalles de post

--- src/index.php
+++ src/index.php
@@ -1,17 +1,24 @@
 <?php
 
+/* Este fichero está sirviendo para manejar las rutas */
+
 $request = $_SERVER['REQUEST_URI'];
-switch ($request) {
+$path = explode("/", $request);
+
+switch ($path[1]) {
   case '':
-  case '/':
     require __DIR__ . '/views/index.php';
     break;
-  case '/posts':
+  case 'posts':
     require __DIR__ . '/controllers/PostControler.php';
 
     $controller = new PostController();
-    $controller->index();
 
+    if (isset($path[2])) {
+      $controller->show($path[2]);
+    } else {
+      $controller->index();
+    }
     break;
   default:
     http_response_code(404);
