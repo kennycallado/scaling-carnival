e4bf1acbdbe43265324cdf94310ac57caedf4997
kenny Callado <kennycallado@hotmail.com>
2021-10-25 20:09:27 +0200

    mejora - Implementa la interface - Logica función show para obtener post - Logica función add para añadir post

--- src/models/PostRepository.php
+++ src/models/PostRepository.php
@@ -1,18 +1,22 @@
 <?php
 
+/* Clases requeridas */
 require_once("models/PostModel.php");
 require_once("Database.php");
 
-class PostRepository
+/* Interfaces requeridas */
+require_once("interfaces/CrudInterface.php");
+
+class PostRepository implements CrudInterface
 {
   public $db;
 
   function __construct()
   {
     $this->db = new Database();
-  }
+  } // __construct()
 
-  function all(): array
+  function getAll(): array
   {
     $posts = [];
     $dbh = $this->db->conn;
@@ -31,9 +35,54 @@ class PostRepository
       }
     } catch (PDOException $e) {
       echo "Error: $e->getMessage()";
-      die("error: Problemas PostRepository all().");
+      die("error: Problemas PostRepository getAll().");
     }
 
     return $posts;
-  }
+  } // getAll()
+
+  function getOne($id): object
+  {
+    $dbh = $this->db->conn;
+
+    try {
+      $query = $dbh->prepare("SELECT * FROM t_posts WHERE id = $id;");
+      $query->execute();
+
+      if ($row = $query->fetch(PDO::FETCH_OBJ)) {
+        $post = new Post(
+          $row->id,
+          $row->user_id,
+          $row->title,
+          $row->content,
+        );
+      }
+    } catch (PDOException $e) {
+      echo "Error: $e->getMessage()";
+      die("error: Problemas PostRepository getOne().");
+    }
+
+    if ($post) return $post;
+    die("Post no existente.");
+  } // getOne()
+
+  function add($values): bool
+  {
+    $dbh = $this->db->conn;
+
+    try {
+      $query = $dbh->prepare("INSERT INTO t_posts(user_id, title, content) values(? ? ?)");
+      $query->execute([$values->user_id, $values->title, $values->content]);
+
+      /* No se como manejar lo que devuelve sql */
+      return true;
+    } catch (PDOException $e) {
+      echo "Error: $e->getMessage()";
+      die("error: Problemas PostRepository all().");
+    }
+  } // add()
+
+  function update($values)
+  {
+  } // update()
 }
